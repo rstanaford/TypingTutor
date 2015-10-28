005057c8fca886eadfc0dc9f2caf1903b720500c
Rich Stanaford <rstanaford@gmail.com>
2015-10-28 14:42:53 -0400

    Skeleton files.

---
+++ src/typingtutor.c
@@ -0,0 +1,10 @@
+/* This is my personal typing tutor.  It will be a study on basic text processing, code timing, and random output on screen via NCurses. */
+
+#include <stdio.h>
+
+int main(){
+
+	printf("Test.\n");
+
+}
+
